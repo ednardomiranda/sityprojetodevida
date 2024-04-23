89eec034d75625b2b04b6a0225183abe699745cc
calango <110404322+ednardomiranda@users.noreply.github.com>
2024-04-23 16:39:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes=document.querySelectorA11(".botao");
+
+for(let i=0; i< botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(j=0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
